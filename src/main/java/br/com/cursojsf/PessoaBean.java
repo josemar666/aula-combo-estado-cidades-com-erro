7e21763592837f20447f49dc9ad57c7ede250064
josemar lobato <josemarlob1@hotmail.com>
2025-07-16 19:48:44 -0300

    meuprojetojsf - erro combo estados/cidades

---
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -0,0 +1,250 @@
+package br.com.cursojsf;
+
+
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AjaxBehaviorEvent;
+import javax.faces.model.SelectItem;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import br.com.dao.DaoGeneric;
+import br.com.entidades.Pessoa;
+import br.com.repository.IDaoPessoa;
+import br.com.repository.IDaoPessoaImple;
+
+@ViewScoped
+@ManagedBean(name = "pessoaBean")
+public class PessoaBean {
+
+      private Pessoa pessoa = new Pessoa ();
+      
+      private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+      
+      private List<Pessoa> pessoas = new ArrayList<Pessoa>();
+      
+      private IDaoPessoa idaoPessoa = new IDaoPessoaImple();
+      
+      private List<SelectItem> estados;
+      
+      
+      
+      public String salvar() {
+    	  
+    	pessoa =  daoGeneric.merge(pessoa);
+    	  
+    	carregarPessoa();
+    	mostrarMsg("cadastrado com sucesso !");
+    	
+    	  return "";
+      }
+
+      private void mostrarMsg(String msg) {
+    	  
+    	  FacesContext context = FacesContext.getCurrentInstance();
+    	  FacesMessage message = new FacesMessage(msg);
+    	  context.addMessage(null, message);
+		
+		
+	}
+
+	public String novo() {
+    	  pessoa = new Pessoa();
+    	  
+    	  return "";
+    	  
+    	  
+      }
+	
+	public String limpar() {
+	
+  pessoa = new Pessoa();
+    	  
+    	  return "";
+		
+		
+	}
+      
+      public String remove() {
+    	  
+    	     	  
+    	  daoGeneric.deletebyId(pessoa);
+    	  
+    	  pessoa = new Pessoa ();
+    	  
+    	  carregarPessoa();
+    	  
+    	  mostrarMsg("removido com sucesso");
+    	  
+    	  return "";
+      }
+      @PostConstruct //com essa notação sempre que for instaciado na memória será carregada a lista
+      public void carregarPessoa() {
+    	  
+    	  pessoas =daoGeneric.getListEntity(Pessoa.class);
+      }
+      
+   
+      
+      
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+			
+	
+
+	public void pesquisaCep(AjaxBehaviorEvent event) {
+		
+	  try {
+		  
+		  URL url = new URL("https://viacep.com.br/ws/" + pessoa.getCep()+"/json/");//monta a url pra consumir o web service
+		  
+		  URLConnection connection = url.openConnection(); //abre a conexão para url
+		  
+		  InputStream is = connection.getInputStream(); // buscar o retorno dos dados da url especificada
+		  
+		  BufferedReader br = new BufferedReader(new InputStreamReader(is , "UTF-8")); //armazena os dados do retorno e UTF-8 pra não dá problemas com acento nos dados
+		  
+		  String cep = "";// variavel que vai receber o buffer de dados
+		  
+		  StringBuilder jsonCep = new StringBuilder();//vai ser usado no método para ler a string na variavel cep
+		  
+		  // usa o while para varrer as linhas da string cep a qual vai receber o buffer dos dados
+		  while ((cep = br.readLine()) != null) {
+			
+			  jsonCep.append(cep);		 
+			  
+			   
+			
+		}
+		  
+		  Pessoa gsonAuxiliar = new Gson().fromJson(jsonCep.toString(),Pessoa.class); //objeto criado para receber os dados da string e mostrar em tela
+		  
+		  
+		  pessoa.setCep(gsonAuxiliar.getCep());
+		  pessoa.setLogradouro(gsonAuxiliar.getLogradouro());
+		  pessoa.setComplemento(gsonAuxiliar.getComplemento());
+		  pessoa.setUnidade(gsonAuxiliar.getUnidade());
+		  pessoa.setBairro(gsonAuxiliar.getBairro());
+		  pessoa.setLocalidade(gsonAuxiliar.getLocalidade());
+		  pessoa.setUf(gsonAuxiliar.getUf());
+		  pessoa.setEstado(gsonAuxiliar.getEstado());
+		  pessoa.setRegiao(gsonAuxiliar.getRegiao());
+		  pessoa.setIbge(gsonAuxiliar.getIbge());
+		  pessoa.setGuia(gsonAuxiliar.getGuia());
+		  pessoa.setDdd(gsonAuxiliar.getDdd());
+		  pessoa.setSiafi(gsonAuxiliar.getSiafi());
+		  
+		  
+		 // System.out.println(gsonAuxiliar);
+		  
+		
+		  
+		  
+		  
+		
+	} catch (Exception ex) {
+		
+		ex.printStackTrace();
+		mostrarMsg("erro ao mostrar o cep !");
+	}
+		
+	}
+	
+	
+   public String deslogar() {
+	   
+	   FacesContext context = FacesContext.getCurrentInstance();
+	   ExternalContext externalContext = context.getExternalContext();
+	   externalContext.getSessionMap().remove("usuarioLogado");
+	   
+	    HttpServletRequest httpServletRequest = (HttpServletRequest) context.getCurrentInstance().getExternalContext().getRequest();
+	    
+	    httpServletRequest.getSession().invalidate();
+	    
+	   
+	   
+	   
+	   return "index.jsf";
+   }
+	
+	
+	public String logar() {
+		
+		Pessoa pessoaUser = idaoPessoa.consultarUsuario(pessoa.getLogin(), pessoa.getSenha());
+		
+		if(pessoaUser != null){	//encontrou o usuario
+			
+			//adicionar o usuario na sessao
+			
+			FacesContext context = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = context.getExternalContext();
+			HttpServletRequest req = (HttpServletRequest) externalContext.getRequest();
+			HttpSession session = req.getSession();
+			
+			session.setAttribute("usuarioLogado", pessoaUser);
+			
+			//externalContext.getSessionMap().put("usuarioLogado", pessoaUser.getLogin());
+			
+			
+			return "pagina.jsf";
+			
+		}	
+		
+		
+		
+		return "index.jsf";
+	}
+	
+	public boolean permiteAcessoComponentes(String acesso) {
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = context.getExternalContext();
+		
+		Pessoa pessoaUse =  (Pessoa)  externalContext.getSessionMap().get("usuarioLogado");
+		
+		return pessoaUse.getPerfilUser().equals(acesso);
+		
+		
+		
+		
+	}
+	
+	public List<SelectItem> getEstados() {
+		estados = idaoPessoa.listaEstados();
+		return estados;
+	}
+
+}
---
+++ src/main/java/br/com/dao/DaoGeneric.java
@@ -0,0 +1,82 @@
+package br.com.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.com.jpautil.JpaUtil;
+
+public class DaoGeneric<T> {
+
+	public void salvar(T entidade) {
+
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+
+		entityManager.persist(entidade);
+
+		entityTransaction.commit();
+
+		entityManager.close();
+
+	}
+
+	public T merge(T entidade) {
+
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+
+		T retorno = entityManager.merge(entidade);
+
+		entityTransaction.commit();
+
+		entityManager.close();
+
+		return retorno;
+
+	}
+
+	public void deletebyId(T entidade) {
+
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+
+		Object id = JpaUtil.getPrimaryKey(entidade);
+
+		entityManager.createQuery("delete from " + entidade.getClass().getCanonicalName() + " where id = " + id)
+				.executeUpdate();
+
+		entityTransaction.commit();
+
+		entityManager.close();
+
+	}
+
+	public List<T> getListEntity(Class<T> entidade) {
+
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+
+		entityTransaction.begin();
+		
+		List<T> retorno = entityManager.createQuery(" from " + entidade.getName()).getResultList();
+		
+		entityTransaction.commit();
+
+		entityManager.close();
+
+        return retorno;
+	}
+
+}
